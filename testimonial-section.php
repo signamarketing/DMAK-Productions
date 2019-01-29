e7426b20843e09eb47ed55036f7e5631356d7c1a
DonVentura <don@signamarketing.com>
2019-01-29 14:13:51 -0700

    Updating
    Trying to update files without overwriting Trevor's changes.

--- testimonial-section.php
+++ testimonial-section.php
@@ -1,9 +1,7 @@
 <div id="hp-testimonial" class="bg clear">
 	<div class="container insidepad">
 		<div id="hp-testimonial-video">
-
-		<?php echo do_shortcode('[fve]'. get_field('testimonial_video_link', 'option') .'[/fve]'); ?>
-
+		<iframe src="https://player.vimeo.com/video/189040143" width="100%" height="377" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
 		</div>
 		<div id="hp-testimonial-content-container">
 			<h4>Ask Our Clients,<br>D-Mak Delivers.</h4>
@@ -15,6 +13,3 @@
 		</div>
 	</div>
 </div>
-
-
-<!-- https://player.vimeo.com/video/189040143 -->
